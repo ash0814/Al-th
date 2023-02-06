cb7f79a8c3f8fbc9a5ae81a086e15356f1069cd0
lewis941001 <lewis1004@naver.com>
2023-02-06 12:56:14 +0900

    [Before]

--- 찌/4.해시/3_전화번호_목록.cpp
+++ 찌/4.해시/3_전화번호_목록.cpp
@@ -1,1 +1,32 @@
-// github fork check
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    return answer;
+}
+
+
+// #include <string>
+// #include <vector>
+// #include <map>
+// #include <algorithm>
+// #include <iostream>
+// #include <cstring>
+
+// using namespace std;
+
+// bool solution(vector<string> phone_book) {
+//     bool answer = true;
+
+//     sort(phone_book.begin(), phone_book.end());
+    
+//     for (int i = 0; i + 1 < phone_book.size(); i++)
+//     {
+//         if (phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].size()))
+//             return (false);
+//     }
+//     return answer;
+// }
