112a4c938d46b44ffaef4f7437140b2f533b94e8
ash <sehyeon014@gmail.com>
2023-02-06 16:37:43 +0900

    더맵게_Heap으로 수정, 통과

--- sehyan/heap/scoville.cpp
+++ sehyan/heap/scoville.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <queue>
 #include <algorithm>
 #include <iostream>
 
@@ -9,23 +10,48 @@ int solution(vector<int> scoville, int K)
 {
   int answer = 0;
   int cnt = 0;
-  sort(scoville.begin(), scoville.end());
-  while (scoville[0] < K)
+  priority_queue<int, vector<int>, greater<int> > q;
+
+  for (int i = 0; i < scoville.size(); i++)
   {
-    if (scoville.size() < 2)
+    q.push(scoville[i]);
+  }
+  while (q.top() < K)
+  {
+    if (q.size() < 2)
       return -1;
-    cnt = scoville[0] + (scoville[1] * 2);
-    scoville.erase(scoville.begin());
-    scoville.erase(scoville.begin());
-    scoville.push_back(cnt);
-    for (int i = 0; i < scoville.size(); i++)
-      cout << scoville[i] << ", ";
-    cout << endl;
+    int tmp1, tmp2;
+    tmp1 = q.top();
+    q.pop();
+    tmp2 = q.top();
+    q.pop();
+    cnt = tmp1 + (tmp2 * 2);
+    q.push(cnt);
     answer++;
-    sort(scoville.begin(), scoville.end());
   }
   return answer;
 }
+// int solution(vector<int> scoville, int K)
+// {
+//   int answer = 0;
+//   int cnt = 0;
+//   sort(scoville.begin(), scoville.end());
+//   while (scoville[0] < K)
+//   {
+//     if (scoville.size() < 2)
+//       return -1;
+//     cnt = scoville[0] + (scoville[1] * 2);
+//     scoville.erase(scoville.begin());
+//     scoville.erase(scoville.begin());
+//     scoville.push_back(cnt);
+//     for (int i = 0; i < scoville.size(); i++)
+//       cout << scoville[i] << ", ";
+//     cout << endl;
+//     answer++;
+//     sort(scoville.begin(), scoville.end());
+//   }
+//   return answer;
+// }
 
 int main()
 {
