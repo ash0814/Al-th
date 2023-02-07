d8d649841b193ac90470e583a206a6ef619c6261
lewis941001 <lewis1004@naver.com>
2023-02-07 16:41:21 +0900

    [JJI_HASH] 3_전화번호_목록

--- 찌/4.해시/3_전화번호_목록.cpp
+++ 찌/4.해시/3_전화번호_목록.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// 문자열 정렬!이 중요하다!
 bool solution(vector<string> phone_book) {
     bool answer = true;
     return answer;
@@ -30,3 +31,34 @@ bool solution(vector<string> phone_book) {
 //     }
 //     return answer;
 // }
+
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+bool compare(string a, string b)
+{
+    int n = strcmp(a.c_str(), b.c_str());
+    if (n > 0)
+        return (false);
+    else
+        return (true);
+}
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+
+    sort(phone_book.begin(), phone_book.end(), compare);
+
+    for (int i = 0; i + 1 < phone_book.size(); i++)
+    {
+        if (phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].size()))
+            return (false);
+    }
+    return answer;
+}
