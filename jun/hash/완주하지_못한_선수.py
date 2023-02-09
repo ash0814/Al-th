b65870bf408f0b2bcf3ae750328c65fe475f4fab
maindishes <65796820+maindishes@users.noreply.github.com>
2023-02-09 20:51:47 +0900

    fix : submodule ssh 로 할시 권한문제 해결

--- jun/hash/완주하지_못한_선수.py
+++
@@ -1,8 +0,0 @@
-# 9:33 ~ 
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    for i,person in enumerate(completion):
-        if person != participant[i]:   
-            return participant[i]
-    return participant[-1]
